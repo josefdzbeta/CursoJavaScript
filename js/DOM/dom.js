63516bb694ec56c814f74444dbebb1e0c1ce3bd2
2DAW17 <2DAW17@esvirgua.es>
2021-10-08 11:46:04 +0200

    actualizado con DOM

---
+++ js/DOM/dom.js
@@ -0,0 +1,21 @@
+/* dom.js
+    Ejercicicios Guiados de DOM.
+    Autor: Jose Angel Fernandez Betancourt
+    licencia: GPL v3 o superior.
+    2021.
+*/
+
+//API -- APLICATION PROGRAMMING INTERFACE
+//DOM -- DOCUMENT OBJECT MODEL
+
+'use strict'
+
+function ejecutar(){
+    // console.log("Funciona")
+    // let input /*variable*/ = document.getElementById('iNombre')
+    // console.log(input)
+    let campos = document.getElementsByTagName('input')
+    console.log(campos)
+    
+}
+
