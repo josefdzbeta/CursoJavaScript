0d67cbd11f04c8b95ffbfc66fcd5de3939f356b4
joseangel <jafb19@outlook.com>
2021-10-19 12:31:28 +0200

    DOM actualizado

--- js/DOM/dom.js
+++ js/DOM/dom.js
@@ -15,7 +15,33 @@ function ejecutar(){
     // let input /*variable*/ = document.getElementById('iNombre')
     // console.log(input)
     let campos = document.getElementsByTagName('input')
-    console.log(campos)
+    console.log(campos[2].value)
+
+    let p = document.querySelectorAll('.c1')
+    console.log(p.outerHTML)
+
+    let spans = document.querySelectorAll('.c1 span')
+    console.log(spans)
+
     
+    //document.getElemtsByClassName
+    
+    //Modificacion del DOM
+    spans[1].innerHTML = 'Viva la Pepa!'
+
+    //Modificar atributos de estilo
+    spans[0].style.color = 'blue'
+    spans[0].style.fontSize = '20pt'
+    setInterval(mover,40) 
 }
+
+let posicion=0
+function mover(){
+   let p = document.getElementById('correCaminos')
+   p.style.left=`${posicion}px`
+   posicion+=1
+}
+    
+
+
 
