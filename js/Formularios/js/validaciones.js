de64061d68f77d38e542d81b3c1f81985a811f39
Jose Angel <jafbeta@outlook.com>
2021-11-01 19:43:08 +0100

    correcto añadido

--- js/Formularios/js/validaciones.js
+++ js/Formularios/js/validaciones.js
@@ -12,52 +12,69 @@ Script para validar un Formulario
 const formulario = document.getElementById('formulario'); // Para acceder al formulario
 const inputs = document.querySelectorAll('#formulario input'); // Para almacenar todos los inputs del formulario 
 
+//Expresiones Regulares
 const expresiones = {
-
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	iNombre: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
+    iApellidos: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
 	iEmail: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	iTelefono: /^\d{9}$/ // 9 numeros.
+	iTelefono: /^\d{9}$/, // 9 numeros.
+    iNIF: /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i //DNI 
 }
 
+const campos = {
+    iNombre: false,
+    iApellidos: false,
+    iEmail: false,
+    iNIF: false,
+    iTelefono: false
+}
 
 const validarFormulario = (evento) => {
     switch (evento.target.name) {
         case "iNombre":
-           validarCampo(expresiones.nombre, e.target, evento.target.name)
+            validarCampo(expresiones.iNombre, evento.target, evento.target.name)
         break
         case "iApellidos":
-            validarCampo(expresiones.iApellidos, e.target, evento.target.name)
+            validarCampo(expresiones.iApellidos, evento.target, evento.target.name)
         break
         case "iEmail":
-            validarCampo(expresiones.iEmail, e.target, evento.target.name)
+            validarCampo(expresiones.iEmail, evento.target, evento.target.name)
         break
         case "iNIF":
-            validarCampo(expresiones.iNIF, e.target, evento.target.name)
+            validarCampo(expresiones.iNIF, evento.target, evento.target.name)
         break
         case "iTelefono":
-            validarCampo(expresiones.iTelefono, e.target, evento.target.name)
+            validarCampo(expresiones.iTelefono, evento.target, evento.target.name)
         break
     }
 }
 const validarCampo = (expresion, input, campo) => {
-    if(expresion.nombre.test(input.value)){ //Comprobamos si la expresion regular es valida 
+    if(expresion.test(input.value)){ //Comprobamos si la expresion regular es valida 
 
-        document.getElementById('iNombre').classList.remove('incorrecto') //Quitamos la clase incorrecto
-        document.getElementById('iNombre').classList.add('correcto')
+        document.getElementById(`${campo}`).classList.remove('incorrecto') //Quitamos la clase incorrecto
+        document.getElementById(`${campo}`).classList.add('correcto')
     
     }else{
-        document.getElementById('iNombre').classList.add('incorrecto')
-        document.getElementById('iNombre').classList.remove('correcto')
+        document.getElementById(`${campo}`).classList.add('incorrecto')
+        document.getElementById(`${campo}`).classList.remove('correcto')
+
     
     }
 }
 inputs.forEach((input) => {
     input.addEventListener('keyup', validarFormulario) // Ejecutar funcion al levantar una tecla
     input.addEventListener('blur', validarFormulario) // Ejecutar funcion al pinchar fuera del formulario 
-        
    
 })
 
+formulario.addEventListener('submit', (evento) => {
+    evento.preventDefault()
 
+    const iPolitica = document.getElementById('iPolitica');
+	if(campos.iNombre && campos.iApellidos && campos.iNIF && campos.iEmail && campos.iTelefono && iPolitica.checked ){
+		formulario.reset();
+	}
+
+})
 
 
