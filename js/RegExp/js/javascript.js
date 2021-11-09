ef64b493a84b6f1de5c08c9a54624936d2a82b1f
Jose Angel <jafb19@outlook.com>
2021-11-09 11:05:09 +0100

    Expresiones Regulares

---
+++ js/RegExp/js/javascript.js
@@ -0,0 +1,17 @@
+/**
+ * @author Jose Angel Fernandez Betancourt
+ * @license GPL V3 2021
+ */
+
+'use strict'
+
+window.onload = iniciar
+
+function iniciar(){
+    document.getElementById('iIP').onblur = comprobar
+}
+
+function comprobar(){
+    console.log("Comprobar...")
+    let exp1 = new RegExp('(\d{1-3}.){3}\d{1-3}')
+}
